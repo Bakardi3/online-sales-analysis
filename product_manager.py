7f1470635f42f4b11af1eb2f39835cba417a3ae0
Boba Bakic <boba.bakic4231@gmail.com>
2025-02-04 11:15:57 +0100

    Added new function

--- product_manager.py
+++ product_manager.py
@@ -20,3 +20,11 @@ class ProductManager(Product):
     def total_value(self):
         total = sum(self.price * self.quantity for self.name in self.products)
         print(f"Total sum:{total}")
+
+    def remove_product(self, name):
+        for product in self.products:
+            if self.name == name:
+                self.products.remove(product)
+                print(f"Product'{name}' is removed.")
+                return
+        print(f"Product ({self.name}) is not find.")
